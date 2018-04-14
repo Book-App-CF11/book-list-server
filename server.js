365a50c8e7723ca859afc3f76fc5ff823b926dc0
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-04-14 15:30:20 -0700

    added server.js updates, want to push to heroku

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const PORT = process.env.PORT || 3000;
-
+const cors = require('cors');
+const PORT = process.env.PORT;
+const pg = require('pg');
 const app = express();
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
+const conString = process.env.DATABASE_URL;
+const client = new pg.Client(conString);
+client.connect();
+client.on('error', err => {
+  console.error(err);
+});
+
+app.get('/books', (req, res) => {
+  client.query(`
+  SELECT title FROM books;`)
+    .then(result => res.send(result.rows));
 });
 
 app.listen(PORT, () => console.log('listening on PORT', PORT));
